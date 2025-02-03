c26146292cdd60dc86c1bf52d1e7c2527f818169
Siddhesh Jadhav <siddheshjadhav60@gmail.com>
2025-02-03 21:03:19 +0530

    change added

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,10 @@
 export const formatTime = (minutes) => {
   const hours = Math.floor(minutes / 60);
   const mins = minutes % 60;
-  const period = hours >= 12 ? "PM" : "AM";
-  const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
-  return `${formattedHours}:${mins.toString().padStart(2, "0")} ${period}`;
+  return `${hours.toString().padStart(2, "0")}:${mins.toString().padStart(2, "0")}`;
 };
 
-// Dynamic background based on selected time
+
 export const getBackgroundGradient = ([start, end]) => {
   return `linear-gradient(to right, 
     #002F6C ${start / 14.4}%, 
